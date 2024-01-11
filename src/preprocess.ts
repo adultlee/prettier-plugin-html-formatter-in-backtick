ba79a0d1b8a8a844adca3ccea431c1d2fae2dd6a
이성인 <devlsi1228@gmail.com>
2024-01-11 23:18:26 +0900

    chore: package title 변경

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -2,9 +2,7 @@ import isWorkingPlugin from "./util/isWorkingPlugin";
 import { print } from "./util/print";
 
 export const preprocess = (code: string) => {
-	if (!isWorkingPlugin(code)) {
-		return code;
-	}
+	if (!isWorkingPlugin(code)) return code;
 
 	return print(code);
 };
