bbde1e6fbdc7052c95bd662e77b9fea93feaf9d7
이성인 <devlsi1228@gmail.com>
2024-01-11 17:03:01 +0900

    test: test 추가

--- test/oneLineTag.test.js
+++ test/oneLineTag.test.js
@@ -3,19 +3,21 @@ import config from "./test-config";
 
 const code = `
 function add(a:number,b:number)    {
-  const element =  /*html */    \`<div><div>hihi
-  </div><div>hihi
-  </div>
+  const element =  /*html */    \`<div>ihi
+ 
+
+
+
+
+
+                             
   </div>\`
   return a + b;
 }
 `;
 
 const formattedCode = `function add(a: number, b: number) {
-    const element = /*html */ \`<div>
-                                  <div>hihi</div>
-                                  <div>hihi</div>
-                                </div>\`;
+    const element = /*html */ \`<div>ihi</div>\`;
     return a + b;
 }
 `;
