8888ff15f6f8be1a797a5692f383fc82665b9ae2
이성인 <devlsi1228@gmail.com>
2024-01-11 16:59:16 +0900

    feat:  해당 코드에서 preprocess 기능을 바탕으로 출력

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -1,9 +1,10 @@
 import isWorkingPlugin from "./util/isWorkingPlugin";
+import { print } from "./util/print";
 
 export const preprocess = (code: string) => {
 	if (!isWorkingPlugin(code)) {
 		return code;
 	}
 
-	return code;
+	return print(code);
 };
