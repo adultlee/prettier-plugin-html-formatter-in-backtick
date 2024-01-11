c4d5205ebabd7282313f8b84af09be2ffa0550dd
이성인 <devlsi1228@gmail.com>
2024-01-11 21:35:14 +0900

    feat: 현재 버전에서 반환하는 prettier 테스트 예제 적용

---
+++ example/example.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+  const element = /*html */ `<div>
+                               <div>hihi</div><div>hihi</div> <div> <div>hihi</div>
+                                 <div>hihi</div>
+                               </div><div>
+                                 <div>hihi</div><div>hihi</div>
+       </div>
+                               <div>
+                  <div>hihi</div><div>hihi</div>
+                               </div>
+   <div>
+            <div>hihi</div>
+                                 <div>            <div>hihi</div> <div>hihi</div>
+               <div>hihi</div>
+  <div>
+          <div>hihi</div>
+                                     <div>hihi</div>
+                                     <div>   <div>hihi</div>
+                                       <div>hihi</div>          </div>
+                                   </div>
+                                   <div>hihi</div>
+                                 </div>
+                                 <div>hihi</div>
+                               </div>
+                             </div>`;
+  return a + b;
+}
